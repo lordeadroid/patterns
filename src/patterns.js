995681ddfd95306785144b1f3e0b3e6ecc690b7b
Rishabh <lordeadroid@users.noreply.github.com>
2023-05-02 17:53:29 +0530

    removed semicolon after function definition

--- src/patterns.js
+++ src/patterns.js
@@ -16,31 +16,31 @@ const lineWidth = function(height, width) {
   }
 
   return lines;
-};
+}
 
 const applyStyle = function(style, lineWidth) {
   return lineWidth.map(style);
-};
+}
 
 const applyStyleGroups = function(styles, lineWidthGroups) {
   return styles.flatMap(function(style, index) {
     return applyStyle(style, lineWidthGroups[index]);
   });
-};
+}
 
 const rectangle = function(height, width) {
   return applyStyle(stars, lineWidth(height, width));
-};
+}
 
 const hollowLine = function(width) {
   return stars(1) + spaces(width - 2) + stars(1);
-};
+}
 
 const hollowRectangle = function(height, width) {
   const lines = [lineWidth(1, width), lineWidth(height - 2, width), lineWidth(1, width)];
 
   return applyStyleGroups([stars, hollowLine, stars], lines); 
-};
+}
 
 const linesPattern = function(height) {
   const styles = [stars, dashes];
@@ -51,14 +51,14 @@ const linesPattern = function(height) {
   }
 
   return designs;
-};
+}
 
 const alternateRectangle = function(height, width) {
   const linesDesigns = linesPattern(height);
   const lines = lineWidth(height, width);
 
   return applyStyleGroups(linesDesigns, lines);
-};
+}
 
 const trianglesWidth = function(height) {
   const lines = [];
@@ -68,11 +68,11 @@ const trianglesWidth = function(height) {
   }
 
   return lines;
-};
+}
 
 const triangle = function(height) {
   return applyStyle(stars, trianglesWidth(height));
-};
+}
 
 exports.triangle = triangle;
 exports.rectangle = rectangle;
